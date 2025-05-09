b134b0e4bc63e37afc7035d38eefdbbf7bd065aa
Athirsha19 <athirsha19@gmail.com>
2025-05-09 19:39:31 +0530

    Add files via upload

---
+++ Javamaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javamaven;
+
+/**
+ *
+ * @author DELL
+ */
+public class Javamaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
